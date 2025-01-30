8909e2331d42facd3ad418c9ad81948ac8b6d7c3
jobueno2024 <job.ueno.2024@gmail.com>
2025-01-31 02:05:54 +0900

    不要なパスを削除

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from routers import httpbasic, security1
+from routers import httpbasic
 
 app = FastAPI()
 
 app.include_router(httpbasic.router, prefix="/httpbasic", tags=["security"])
-# app.include_router(security1.router, prefix="/security1", tags=["security"])
 
 @app.get("/")
 async def root():
